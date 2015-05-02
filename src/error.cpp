3da9fc99747d563a6aa5acb5ede66d6d38a71f2c
Ying-Ruei Liang (KK) <thumbd03803@gmail.com>
2015-05-02 16:29:48 +0800

    Fix error message

--- src/error.cpp
+++ src/error.cpp
@@ -12,9 +12,9 @@ getError(int errorno)
 		/* Since getopt() show message itself, we leave message empty */
 		return (char*) "";
 	case ERROR_MULTI_OPERATION:
-		return (char*) _("Ambiguous operation options, please don't \
-			mix different operations in one command");
+		return (char*) _("Ambiguous operation options, please don't "
+			"mix different operations in one command");
 	default:
-		return (char*) _("Can not find corresponded error message, \
-			please file a bug to developers, thanks!"); }
+		return (char*) _("Can not find corresponded error message, "
+			"please file a bug to developers, thanks!"); }
 }
