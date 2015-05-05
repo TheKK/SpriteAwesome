420bdc20731c55a4c52e7acae30347cf7d4a1314
Ying-Ruei Liang (KK) <thumbd03803@gmail.com>
2015-05-05 21:02:23 +0800

    Add CXXFLAGS for FakeIt

--- mk/tests.mk
+++ mk/tests.mk
@@ -2,6 +2,8 @@ TEST_LIBS := -lgtest -pthread
 
 TEST_SRC_DIR := test
 
+CXXFLAGS += -I $(PWD)/FakeIt/include -I $(PWD)/FakeIt/config/gtest
+
 TEST_SRCS := $(wildcard $(TEST_SRC_DIR)/*.cpp)
 TEST_OBJS := $(addprefix $(BUILD_DIR)/, $(TEST_SRCS:.cpp=.o))
 TEST_DEPS := $(OBJS:.o=.d)
